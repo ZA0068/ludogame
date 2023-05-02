f2432eb487c8ded32121c6e928d3d47151f87197
Zain Ahmed <zaahm18@student.sdu.dk>
2023-05-02 15:07:07 +0200

    begun to test

--- libraries/board/src/lib.rs
+++ libraries/board/src/lib.rs
@@ -1,3 +1,28 @@
+pub struct Board {
+    pub home: [i8; 16],
+    pub goal: [i8; 4],
+    pub outside: [i8; 52],
+    pub inside: [i8; 20],
+    pub globe: [i8; 8],
+    pub star: [i8; 8],
+}
+
+impl Board {
+    pub fn new() -> Board {
+        let outside: [i8; 52] = (0..52).map(|i| i as i8).collect::<Vec<i8>>().try_into().unwrap();
+        let inside: [i8; 20] = (52..72).map(|i| i as i8).collect::<Vec<i8>>().try_into().unwrap();
+
+        Board {
+            home: [-1; 16],
+            goal: [99; 4],
+            outside,
+            inside,
+            globe: [0, 8, 13, 21, 26, 34, 39, 47],
+            star: [5, 12, 18, 25, 31, 38, 44, 51],
+        }
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
