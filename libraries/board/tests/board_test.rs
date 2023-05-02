303a1d9cb4a71d45766d37917ab096ac02212ea4
Zain Ahmed <zaahm18@student.sdu.dk>
2023-05-02 17:01:18 +0200

    instantiation succesful for board

--- libraries/board/tests/board_test.rs
+++ libraries/board/tests/board_test.rs
@@ -1,12 +1,16 @@
 use board::Board;
+use std::any::TypeId;
 
 #[cfg(test)]
 mod tests {
+    use std::any::Any;
+
     use super::*;
 
     #[test]
     fn create_a_board_test() {
         let board = Board::new();
-        assert_eq!(board.home(), [-1; 16]);
+        assert_eq!(TypeId::of::<Board>(), board.type_id());
     }
+    
 }
