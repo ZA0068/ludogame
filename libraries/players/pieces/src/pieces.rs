591134d45153e1b2961870c08cfc4d46f15543e1
Zain Ahmed <zaahm18@student.sdu.dk>
2023-05-02 19:23:23 +0200

    updates

--- libraries/players/pieces/src/pieces.rs
+++ libraries/players/pieces/src/pieces.rs
@@ -1,14 +1,31 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod pieces {
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub struct State {
+        home: bool,
+        goal: bool,
+        safe: bool,
+        invincible: bool,        
+        position: u8,
+    }
+    pub struct Piece {
+        id: u8,
+        state: State,
+    }
+    
+    impl Piece {
+        pub fn new(id: u8) -> Piece {
+            Piece {
+                id: id,
+                state: State {
+                    home: true,
+                    goal: false,
+                    safe: false,
+                    invincible: false,
+                    position: 0,
+                }
+            }
+        }
     }
 }
+
+pub use pieces::Piece;
---
+++ libraries/players/pieces/tests/pieces_test.rs
@@ -0,0 +1,10 @@
+use piece::Piece;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
