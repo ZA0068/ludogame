edf6267eff77f3f6f3b06f65b04a63b12841287c
Zain Ahmed <zaahm18@student.sdu.dk>
2023-05-02 14:23:43 +0200

    initilization of project

---
+++ libraries/board/target/debug/deps/board-87c4fda77c5b7ae8.d
@@ -0,0 +1,7 @@
+C:\Users\zaina\OneDrive\Dokumenter\2nd_semester\Artificial_intelligence_tools\Report\Ludo Game code\rust\ludogame\libraries\board\target\debug\deps\board-87c4fda77c5b7ae8.rmeta: src\lib.rs
+
+C:\Users\zaina\OneDrive\Dokumenter\2nd_semester\Artificial_intelligence_tools\Report\Ludo Game code\rust\ludogame\libraries\board\target\debug\deps\libboard-87c4fda77c5b7ae8.rlib: src\lib.rs
+
+C:\Users\zaina\OneDrive\Dokumenter\2nd_semester\Artificial_intelligence_tools\Report\Ludo Game code\rust\ludogame\libraries\board\target\debug\deps\board-87c4fda77c5b7ae8.d: src\lib.rs
+
+src\lib.rs:
